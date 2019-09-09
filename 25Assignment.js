2c70e5f94aba03af05e12567aee1bc9d4eaaf1cf
Krantesh Singh <34528823+KranteshSingh@users.noreply.github.com>
2019-09-09 06:27:02 +0530

    Solved

---
+++ 25Assignment.js
@@ -0,0 +1,16 @@
+/*
+Complete function multiplyMagic() to multiply two elements. 
+The function should never return an error, irrespective of input passed and should return 1 in case 
+of any kind of error or problem with input. Use default parameters for the same. 
+sample input : multiplyMagic(2,3)
+output - 6
+
+
+*/
+
+let multiplyMagic = (a = 1, b = 1) => {
+  // start your code here.
+  return a * b;
+};
+
+console.log(multiplyMagic(2));
